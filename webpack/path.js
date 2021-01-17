1c8fbee69bdab6ec5efc12193eb3236c9eb7c395
Wanlin-Lu <luwanlin@126.com>
2021-01-17 14:09:13 +0800

    setup webpack

---
+++ webpack/path.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+"use strict"
+
+const path = require('path')
+
+module.exports = {
+  root: path.resolve(__dirname, '../'),
+  src: path.resolve(__dirname, '../', 'src'),
+  output: path.resolve(__dirname, '../', 'build'),
+  entry: path.resolve(__dirname, '../', 'src/js/index.js'),
+  template: path.resolve(__dirname, '../', 'src/index.html'),
+  favicon: path.resolve(__dirname, '../', 'src/public/favicon.ico'),
+  images: path.resolve(__dirname, '../', 'src/images'),
+  imagesFolder: 'images',
+  fontsFolder: 'fonts',
+  cssFlolder: 'css',
+  jsFolder: 'js'
+}
